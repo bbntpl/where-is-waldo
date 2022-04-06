8e655d3a41ba237f611a1254fa50540c6426c981
bvrbryn445 <law911012eqw@gmail.com>
2022-04-05 23:59:55 -0400

    remotely save changes

---
+++ src/Assets/selector_colors.js
@@ -0,0 +1,44 @@
+const DIFF_SELECTOR = [
+    {
+        icon: 'otter',
+        iconcolor: '#0021a4',
+        shadowcolor: '#d7dfff',
+        innerdark: '#1638bd',
+        innerlight: '#6c89fb',
+        inneredge: '#7a93f2',
+        outercolor: '#869efd',
+        outercolor2: '#1a48ff'
+    },
+    {
+        icon: 'spider',
+        iconcolor: '#008e34',
+        shadowcolor: '#99fcbd',
+        innerdark: '#24a855',
+        innerlight: '#54e289',
+        inneredge: '#56e58b',
+        outercolor: '#4dc077',
+        outercolor2: '#00bf46'
+    },
+    {
+        icon: ['fab', 'wolf-pack-battalion'],
+        iconcolor: '#b97e00',
+        shadowcolor: '#fff1d2',
+        innerdark: '#f1b22a',
+        innerlight: '#face6f',
+        inneredge: '#fbce6c',
+        outercolor: '#fed887',
+        outercolor2: '#ffb61a'
+    },
+    {
+        icon: 'dragon',
+        iconcolor: '#c90000',
+        shadowcolor: '#ffd6d6',
+        innerdark: '#ff2c2c',
+        innerlight: '#ff4141',
+        inneredge: '#fe4d4d',
+        outercolor: '#ff7878',
+        outercolor2: '#ff1919'
+    }
+]
+
+export { DIFF_SELECTOR }
